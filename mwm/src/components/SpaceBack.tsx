b73c5b351a593fb09402cc32ee971cf5c5aa2edc
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-04-11 22:46:25 +0900

    [Style] spacebackground color change

--- mwm/src/components/SpaceBack.tsx
+++ mwm/src/components/SpaceBack.tsx
@@ -1,7 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { useRef } from "react";
-import TubiSpline from "./Tubi";
 
 const StarField = () => {
   const geometry = new THREE.BufferGeometry();
@@ -20,7 +19,7 @@ const StarField = () => {
   const starField = useRef<THREE.Points>(null);
 
   useFrame(({ clock, camera }) => {
-    const rot = clock.elapsedTime * 0.7;
+    const rot = clock.elapsedTime * 0.5;
     const radian = (rot * Math.PI) / 180;
     camera.position.x = 1000 * Math.sin(radian);
     camera.position.z = 1000 * Math.cos(radian);
@@ -39,7 +38,7 @@ const SpaceBack = () => {
   return (
     <div className="overflow-hidden w-screen h-screen flex justify-center items-center">
       <Canvas
-        className="bg-gradient-to-br from-indigo-900 to-neutral-800 w-full h-full"
+        className="bg-gradient-to-b from-gray-900 to-slate-800 w-full h-full"
         onCreated={({ gl }) => {
           gl.setClearColor(0x000000, 0);
         }}
@@ -52,82 +51,9 @@ const SpaceBack = () => {
         }}
       >
         <StarField />
-        {/* <TubiSpline /> */}
       </Canvas>
     </div>
   );
 };
 
 export default SpaceBack;
-// import * as THREE from "three";
-// import { useEffect, useRef } from "react";
-// import TubiSpline from "./Tubi";
-
-// const StarField = () => {
-//   const geometry = new THREE.BufferGeometry();
-//   const positions = new Float32Array(10000 * 3);
-
-//   for (let i = 0; i < 10000; i++) {
-//     const i3 = i * 3;
-//     positions[i3 + 0] = THREE.MathUtils.randFloatSpread(2000);
-//     positions[i3 + 1] = THREE.MathUtils.randFloatSpread(2000);
-//     positions[i3 + 2] = THREE.MathUtils.randFloatSpread(2000);
-//   }
-
-//   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
-
-//   const material = new THREE.PointsMaterial({ color: 0xffffff });
-//   const starField = useRef<THREE.Points>(null);
-
-//   useEffect(() => {
-//     const animate = () => {
-//       if (!starField.current) return;
-//       const rot = performance.now() * 0.0001;
-//       const radian = (rot * Math.PI) / 180;
-//       starField.current.position.x = 1000 * Math.sin(radian);
-//       starField.current.position.z = 1000 * Math.cos(radian);
-//       starField.current.position.y = 0;
-//       starField.current.lookAt(new THREE.Vector3(0, 0, 0));
-//       requestAnimationFrame(animate);
-//     };
-//     animate();
-//   }, []);
-
-//   return (
-//     <points ref={starField} geometry={geometry} material={material}>
-//       <fog attach="fog" args={[0xaaaaaa, 50, 2000]} />
-//     </points>
-//   );
-// };
-
-// const SpaceBack = () => {
-//   return (
-//     <div className="w-screen h-screen flex justify-center items-center">
-//       <canvas
-//         className="bg-gradient-to-br from-blue-900 to-neutral-800 w-full h-full"
-//         ref={(el) => {
-//           if (!el) return;
-//           const renderer = new THREE.WebGLRenderer({ canvas: el });
-//           renderer.setClearColor(0x000000, 0);
-//           const camera = new THREE.PerspectiveCamera(
-//             70,
-//             window.innerWidth / window.innerHeight,
-//             0.1,
-//             2000
-//           );
-//           camera.position.set(0, 0, 1000);
-//           const scene = new THREE.Scene();
-//           scene.fog = new THREE.Fog(0xaaaaaa, 50, 2000);
-//           scene.add(camera, new StarField(), new TubiSpline());
-//           const animate = () => {
-//             renderer.render(scene, camera);
-//             requestAnimationFrame(animate);
-//           };
-//           animate();
-//         }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default SpaceBack;
