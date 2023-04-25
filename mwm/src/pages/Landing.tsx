5c011e254a63699d55a94cb0faaf2a0acae6c3f6
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-04-25 15:33:42 +0900

    [Feat] create Loading page

--- mwm/src/pages/Landing.tsx
+++ mwm/src/pages/Landing.tsx
@@ -1,12 +1,15 @@
 import SpaceBack from "../components/SpaceBack";
 import TubiSpline from "../components/Mozi";
 import LandingText from "../components/LandingText";
+import Loading from "../components/Loading";
+import { useMemo } from "react";
 
 const Landing = () => {
   return (
     <div className="relative overflow-hidden w-screen h-screen flex justify-center items-center">
       <SpaceBack />
       <LandingText />
+      <Loading />
       <TubiSpline />
     </div>
   );
