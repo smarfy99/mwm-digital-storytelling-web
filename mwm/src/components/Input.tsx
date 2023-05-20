ea0595bb072928cabe37eb48b1ec2fafedee3b2d
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-05-20 20:23:52 +0900

    [Feat] Message type 정의

--- mwm/src/components/Input.tsx
+++ mwm/src/components/Input.tsx
@@ -1,6 +1,20 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import firebase from 'firebase/app';
+import {getDatabase} from 'firebase/database';
+
+const database = getDatabase();
+
+type Message = {
+    name: string;
+    text: string;
+    timestamp: number;
+}
+
 const Input = () => {
   return (
     <div> 
+        <input type="text" className="flex outline-0 underline-offset-auto" />
         <input type="text" className="flex outline-0 underline-offset-auto" />
         <button type="submit" className="flex text-slate-100 ">
             확인
