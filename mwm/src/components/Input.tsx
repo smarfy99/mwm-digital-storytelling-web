0755cf427f309261436ebcd4cd54fd27b981d03c
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-05-21 23:32:06 +0900

    [Feat] input, user ref 추가

--- mwm/src/components/Input.tsx
+++ mwm/src/components/Input.tsx
@@ -1,26 +1,36 @@
-import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import firebase from 'firebase/app';
-import {getDatabase} from 'firebase/database';
+import { getDatabase, ref, push } from 'firebase/database';
 
 const database = getDatabase();
 
 type Message = {
-    name: string;
-    text: string;
-    timestamp: number;
-}
+  name: string;
+  text: string;
+  timestamp: number;
+};
 
 const Input = () => {
-  return (
-    <div> 
-        <input type="text" className="flex outline-0 underline-offset-auto" />
-        <input type="text" className="flex outline-0 underline-offset-auto" />
-        <button type="submit" className="flex text-slate-100 ">
-            확인
-        </button>
-    </div>
-  )
-}
+  const inputRef = useRef<HTMLInputElement>(null);
+  const userRef = useRef<HTMLInputElement>(null);
+  const [messages, setMessages] = useState<Message[]>([]);
 
-export default Input
+  const sendMessage = () => {
+    const input = inputRef.current?.value;
+    const user = userRef.current?.value;
+
+  }
+
+  return (
+    <div>
+      <input type="text" ref={userRef} className="flex outline-0 underline-offset-auto" />
+      <input type="text" ref={inputRef} className="flex outline-0 underline-offset-auto" />
+      <button type="submit" className="flex text-slate-100 ">
+        확인
+      </button>
+    </div>
+  );
+};
+
+export default Input;
