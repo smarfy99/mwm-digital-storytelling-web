6ac5356bfbc1caaa54d1f3636f4ccc4f87270ffb
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-05-22 19:34:15 +0900

    [Feat]get data

--- mwm/src/components/Input.tsx
+++ mwm/src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { app, analytics } from '../firebase';
 import { getDatabase, ref, push, onValue, set } from 'firebase/database';
@@ -12,63 +12,67 @@ type Message = {
 };
 
 const Input = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const userRef = useRef<HTMLInputElement>(null);
-  const [messages, setMessages] = useState<Message[]>([]);
+  // const inputRef = useRef<HTMLInputElement>(null);
+  // const userRef = useRef<HTMLInputElement>(null);
+  // const [messages, setMessages] = useState<Message[]>([]);
 
-  const sendMessage = () => {
-    const input = inputRef.current?.value;
-    const user = userRef.current?.value;
-
-    if (input) {
-      const messages: Message = {
-        name: user,
-        text: input,
-        timestamp: Date.now(),
-      };
-    }
-  };
-
-  // message를 firebase에 넣기
-  const newMessageRef = push(ref(database, 'messages'), messages);
-
-  // clear input
-  inputRef.current!.value = '';
-
-  // firebase에서 다시 불러오기
-  const messageRef = ref(database, 'messages');
-  onValue(messageRef, (snapshot) => {
-    const data = snapshot.val();
-    const messageList: Message[] = [];
-
-    for (let key in data) {
-      messageList.push({
-        name: data[key].name,
-        text: data[key].text,
-        timestamp: data[key].timestamp,
-      });
-    }
-    setMessages(messageList);
+  useEffect(() => {
+    console.log(database);
   });
+
+  // const sendMessage = () => {
+  //   const input = inputRef.current?.value;
+  //   const user = userRef.current?.value;
+
+  //   if (input) {
+  //     const messages: Message = {
+  //       name: user,
+  //       text: input,
+  //       timestamp: Date.now(),
+  //     };
+  //   }
+  // };
+
+  // // message를 firebase에 넣기
+  // const newMessageRef = push(ref(database, 'messages'), messages);
+
+  // // clear input
+  // inputRef.current!.value = '';
+
+  // // firebase에서 다시 불러오기
+  // const messageRef = ref(database, 'messages');
+  // onValue(messageRef, (snapshot) => {
+  //   const data = snapshot.val();
+  //   const messageList: Message[] = [];
+
+  //   for (let key in data) {
+  //     messageList.push({
+  //       name: data[key].name,
+  //       text: data[key].text,
+  //       timestamp: data[key].timestamp,
+  //     });
+  //   }
+  //   setMessages(messageList);
+  // });
 
   return (
     <div>
       <div>
-        {messages.map((message, index) => (
+        {/* {messages.map((message, index) => (
           <div key={index}>
             <span>{message.name}: </span>
             <span>{message.text}</span>
           </div>
-        ))}
+        ))} */}
       </div>
       <div>
-        <form>
+        {/* <form>
           <input type="text" ref={userRef} className="flex outline-0 underline-offset-auto" />
           <input type="text" ref={inputRef} className="flex outline-0 underline-offset-auto" />
           <button type="submit" onClick={sendMessage} className="flex text-slate-100 ">
             확인
           </button>
-        </form>
+        </form> */}
       </div>
     </div>
   );
