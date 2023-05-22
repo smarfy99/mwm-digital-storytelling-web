25f6c43fe2f01f47c110b83c5bf836c537215cc3
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-05-23 02:48:13 +0900

    [Feat] DataSphere 추가

--- mwm/src/components/DataSphere.tsx
+++ mwm/src/components/DataSphere.tsx
@@ -1,7 +1,32 @@
-const DataSphere = () => {
-  return (
-    <div>DataSphere</div>
-  )
-}
+import { useEffect } from 'react';
 
-export default DataSphere
+// 외부 패키지
+import TagCloud from 'TagCloud';
+
+const DataSphere = () => {
+  useEffect(() => {
+    return () => {
+      const container: string = '.tagcloud';
+      const texts: string[] = ['jiwon', '직우', '사랑', '안녕', '나는', '지원', '이야'];
+
+      const options: object = {
+        radius: 300,
+        maxSpeed: 'normal',
+        initSpeed: 'normal',
+        keep: true,
+      };
+
+      TagCloud(container, texts, options);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="relative flex justify-center items-center w-50 h-50">
+        <span className="inline-block text-white"></span>
+      </div>
+    </>
+  );
+};
+
+export default DataSphere;
