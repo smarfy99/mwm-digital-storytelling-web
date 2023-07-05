5a5d2f2cbac5d0b004abf96f6eb10c91f92e70cd
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-06 00:01:22 +0900

    [Feat] PhotoBooth page 추가

---
+++ mwm/src/pages/PhotoBooth.tsx
@@ -0,0 +1,11 @@
+import Camera from '../components/Camera';
+
+const PhotoBooth = () => {
+  return (
+    <div className="relative w-screen h-screen flex justify-center items-center">
+      <Camera />
+    </div>
+  );
+};
+
+export default PhotoBooth;
