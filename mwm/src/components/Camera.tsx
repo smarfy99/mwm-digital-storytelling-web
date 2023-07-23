7e037c17233820cc2803d19ab70c94a5c7820ce0
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-24 02:07:36 +0900

    [Feat] firebase에서 인생네컷 불러오기

--- mwm/src/components/Camera.tsx
+++ mwm/src/components/Camera.tsx
@@ -1,15 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import photoFrame from '../assets/photoframe.png';
 import { storage } from '../firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
 
 // 웹 시작
 const MoziCamera = () => {
+  // mergedImageURL에 이미지 URL 저장
+  const [mergedImageURL, setMergedImageURL] = useState<string | null>(null);
 
+  useEffect(() => {
+    // Firebase Storage에서 이미지 불러오기
+    const imageRef = ref(storage, 'images/mergedImage.jpg');
+
+    getDownloadURL(imageRef)
+      .then((url) => {
+        setMergedImageURL(url);
+      })
+      .catch((error) => {
+        console.error('이미지 불러오기 실패 : ', error);
+      });
+  }, []);
 
   return (
     <div className="">
       <div className="">
-        <img src={photoFrame} alt="photoFrame"></img>
+        {mergedImageURL ? <img src={mergedImageURL} alt="mergedImage" /> : <p>이미지 불러오는 중...</p>}
+        <input id="text" type="text"></input>
+        <div className="w-[300px] h-[300px] mt-7"></div>
       </div>
     </div>
   );
