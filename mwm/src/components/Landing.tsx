c3b5492ee283014cc9af9a5cd4b20858031264dc
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-25 22:35:15 +0900

    [Feat] Landing router -> Opening

--- mwm/src/components/Landing.tsx
+++ mwm/src/components/Landing.tsx
@@ -3,43 +3,17 @@ import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
   const navigate = useNavigate();
-  const [clicked, setClicked] = useState<boolean>(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
 
   const handleVideoClick = () => {
     navigate('/opening');
   };
 
-
-
-
-  useEffect(() => {
-    // 비디오 클릭하고, 9초 후에 화면 이동
-    let timer: NodeJS.Timeout;
-
-    if (clicked) {
-      timer = setTimeout(() => {
-        navigate('/camera');
-      }, 9000);
-    }
-
-    // timer unmount
-    return () => clearTimeout(timer);
-  }, [clicked, navigate]);
-
-  useEffect(() => {
-    //custom controls
-    const videoElement = videoRef.current;
-    if (videoElement) {
-      videoElement.controls = false; //hide default controls
-    }
-  });
-
   return (
     <>
       <div className="relative w-screen h-screen ">
         <video
-          ref={videoRef}
+          autoPlay
+          loop
           onClick={handleVideoClick}
           src="../../public/high.mp4"
           className="absolute top-0 left-0 object-cover"
