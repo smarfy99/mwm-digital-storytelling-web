2f66f1f207aa1f50074570dccd5d7458b92ee34f
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-25 23:02:01 +0900

    [Feat] router sub2

---
+++ mwm/src/components/Love.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Love = () => {
+  return (
+    <div>Love</div>
+  )
+}
+
+export default Love
---
+++ mwm/src/components/Sub2.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+
+const Sub2 = () => {
+  const navigate = useNavigate();
+
+  const handleVideoClick = () => {
+    navigate('/love');
+  };
+
+  return (
+    <>
+      <div className="relative w-screen h-screen ">
+        <video
+          autoPlay
+          loop
+          onClick={handleVideoClick}
+          src="../../public/high.mp4"
+          className="absolute top-0 left-0 object-cover"
+        />
+        <source src="../../public/high.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </div>
+    </>
+  );
+};
+
+export default Sub2;
