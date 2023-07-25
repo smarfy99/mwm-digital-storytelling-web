1d35111af82c6e278b7166a12bd0d52b67ca69a8
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-07-25 21:47:30 +0900

    [Feat] video태그 controls 속성 시 progress bar 없애기 / 클릭시 영상 재생, 영상 재생 9초 후 다른 컴포넌트로 넘어가기

--- mwm/src/components/Landing.tsx
+++ mwm/src/components/Landing.tsx
@@ -1,25 +1,66 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
   const navigate = useNavigate();
+  const [clicked, setClicked] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleVideoClick = () => {
+    setClicked(true);
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      if (videoElement.paused) {
+        videoElement.play();
+      } else {
+        videoElement.pause();
+      }
+    }
+  };
+
+  // useEffect(() => {
+  //   // 5초 후, camera 컴포넌트로 이동
+  //   const timer = setTimeout(() => {
+  //     navigate('/camera');
+  //   }, 5000);
+
+  //   // timer unmount
+  //   return () => clearTimeout(timer);
+  // }, [navigate]);
 
   useEffect(() => {
-    // 5초 후, camera 컴포넌트로 이동
-    const timer = setTimeout(() => {
-      navigate('/camera');
-    }, 5000);
+    // 비디오 클릭하고, 9초 후에 화면 이동
+    let timer: NodeJS.Timeout;
+
+    if (clicked) {
+      timer = setTimeout(() => {
+        navigate('/camera');
+      }, 9000);
+    }
 
     // timer unmount
     return () => clearTimeout(timer);
-  }, [navigate]);
+  }, [clicked, navigate]);
+
+  useEffect(() => {
+    //custom controls
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      videoElement.controls = false; //hide default controls
+    }
+  });
 
   return (
     <>
       <div className="relative w-screen h-screen ">
-        <video autoPlay src="../../public/high.mp4" className="absolute top-0 left-0 object-cover" />
-          <source src="../../public/high.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
+        <video
+          ref={videoRef}
+          onClick={handleVideoClick}
+          src="../../public/high.mp4"
+          className="absolute top-0 left-0 object-cover"
+        />
+        <source src="../../public/high.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
       </div>
     </>
   );
