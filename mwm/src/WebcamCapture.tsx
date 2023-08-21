aa9db94387f5e175dec5d66641fcac35070a1623
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-08-22 00:09:59 +0900

    [Feat] 파이어베이스에서 이미지 불러오기

--- mwm/src/WebcamCapture.tsx
+++ mwm/src/WebcamCapture.tsx
@@ -2,8 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { createGestureRecognizer } from './mediapipe';
 import { GestureRecognizer } from '@mediapipe/tasks-vision';
 import { storage } from './firebase';
-import { ref, getDownloadURL } from 'firebase/storage';
-import { uploadBytes } from 'firebase/storage';
+import { ref, getDownloadURL, uploadBytes, listAll } from 'firebase/storage';
 
 const WebcamCapture = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null); //렌더링되는 video태그
@@ -12,6 +11,10 @@ const WebcamCapture = () => {
   const [gestureRecognizer, setGestureRecognizer] = useState<GestureRecognizer | undefined>(); //제스처 만드는 친구 실행상태
   const [webcamRunning, setWebcamRunning] = useState<boolean>(false); // 웹캠 실행상태
   const [imageList, setImageList] = useState<String[]>([]);
+
+  let cnt = 0;
+  const newStorage = storage;
+  const imageListRef = ref(newStorage, 'images/');
 
   useEffect(() => {
     const initializeCamera = async () => {
@@ -40,12 +43,25 @@ const WebcamCapture = () => {
     }
   }, [gestureRecognizer]);
 
+  useEffect(() => {
+    if(cnt >= 5){
+      listAll(imageListRef).then((response) => {
+        response.items.forEach((item,index) => {
+          if( 1 <= index && index < 5){
+            getDownloadURL(item).then((url) => {
+              setImageList((prev) => [...prev, url]);
+            })
+          }
+        })
+      })
+    }
+  }, [cnt])
+
   let lastVideoTime = -1;
   let results: any;
 
   const uploadImageToFirebaseStorage = async (blob: Blob, cnt: number) => {
     try {
-      const newStorage = storage;
       const imageRef = ref(newStorage, `images/image${cnt}.png`); // Firebase Storage의 루트 참조
       uploadBytes(imageRef, blob).then((snapshot) => {
         getDownloadURL(snapshot.ref).then((url) => {
@@ -61,7 +77,7 @@ const WebcamCapture = () => {
   const predictWebcam = async () => {
     if (videoRef.current && gestureRecognizer) {
       let nowInMs = Date.now();
-      let cnt = 0;
+      cnt = 0;
       if (videoRef.current.currentTime !== lastVideoTime) {
         lastVideoTime = videoRef.current.currentTime;
         results = gestureRecognizer.recognizeForVideo(videoRef.current, nowInMs);
@@ -94,6 +110,9 @@ const WebcamCapture = () => {
       }
     }
   };
+
+
+
   return (
     <div>
       <video ref={videoRef} autoPlay></video>
