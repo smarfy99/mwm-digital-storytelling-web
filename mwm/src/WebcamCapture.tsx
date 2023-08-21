4a9200fba11ca7f7dda019337bd5cb446f86a13e
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-08-21 23:42:39 +0900

    [Feat] web에서 웹캠 인식

---
+++ mwm/src/WebcamCapture.tsx
@@ -0,0 +1,105 @@
+import { useEffect, useRef, useState } from 'react';
+import { createGestureRecognizer } from './mediapipe';
+import { GestureRecognizer } from '@mediapipe/tasks-vision';
+import { storage } from './firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
+import { uploadBytes } from 'firebase/storage';
+
+const WebcamCapture = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null); //렌더링되는 video태그
+  const canvasRef = useRef<HTMLCanvasElement | null>(null); //렌더링되는 canvas태그
+
+  const [gestureRecognizer, setGestureRecognizer] = useState<GestureRecognizer | undefined>(); //제스처 만드는 친구 실행상태
+  const [webcamRunning, setWebcamRunning] = useState<boolean>(false); // 웹캠 실행상태
+  const [imageList, setImageList] = useState<String[]>([]);
+
+  useEffect(() => {
+    const initializeCamera = async () => {
+      //웹캠 켜고, 제스쳐 만드는 라이브러리 initialize
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        }); //웹캠 스트림
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+          setWebcamRunning(true);
+
+          const recognizer = await createGestureRecognizer();
+          setGestureRecognizer(recognizer);
+        }
+      } catch (error) {
+        console.error('Error initializing camera:', error);
+      }
+    };
+    initializeCamera();
+  }, []);
+
+  useEffect(() => {
+    if (webcamRunning && gestureRecognizer) {
+      predictWebcam();
+    }
+  }, [gestureRecognizer]);
+
+  let lastVideoTime = -1;
+  let results: any;
+
+  const uploadImageToFirebaseStorage = async (blob: Blob, cnt: number) => {
+    try {
+      const newStorage = storage;
+      const imageRef = ref(newStorage, `images/image${cnt}.png`); // Firebase Storage의 루트 참조
+      uploadBytes(imageRef, blob).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        });
+      });
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      return null;
+    }
+  };
+
+  const predictWebcam = async () => {
+    if (videoRef.current && gestureRecognizer) {
+      let nowInMs = Date.now();
+      let cnt = 0;
+      if (videoRef.current.currentTime !== lastVideoTime) {
+        lastVideoTime = videoRef.current.currentTime;
+        results = gestureRecognizer.recognizeForVideo(videoRef.current, nowInMs);
+        if (results && results?.gestures?.[0]?.[0]?.categoryName === 'Closed_Fist') {
+          const canvas = canvasRef.current;
+          const video = videoRef.current;
+
+          if (canvas && video) {
+            const canvasContext = canvas.getContext('2d');
+            if (canvasContext) {
+              canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height); //canvas에 capture
+              const imageBlob = await new Promise<Blob | null>((resolve) => {
+                canvas.toBlob((blob) => {
+                  resolve(blob);
+                });
+              });
+              //여기서 imageBlob 사용가능
+              if (imageBlob) {
+                cnt++;
+                //이미지 Blob을 파이어베이스 스토리지에 업로드
+                const imageURL = await uploadImageToFirebaseStorage(imageBlob, cnt);
+                //imageURL을 활용한 추가 작업 수행 가능
+              }
+            }
+          }
+        }
+      }
+      if (webcamRunning) {
+        window.requestAnimationFrame(predictWebcam); // Continue calling predictWebcam
+      }
+    }
+  };
+  return (
+    <div>
+      <video ref={videoRef} autoPlay></video>
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
+};
+
+export default WebcamCapture;
---
+++ mwm/src/mediapipe.ts
@@ -0,0 +1,15 @@
+import { GestureRecognizer, FilesetResolver } from '@mediapipe/tasks-vision';
+export const createGestureRecognizer = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm',
+  );
+  const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        'https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task',
+      delegate: 'GPU',
+    },
+    runningMode: 'VIDEO',
+  });
+  return gestureRecognizer;
+};
