aea28f9109c742032cfaea6ab5d1a8942578608f
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-08-24 01:48:54 +0900

    [Feat] sub일때만 loop되게

--- mwm/src/components/Angry.tsx
+++ mwm/src/components/Angry.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 
 const Angry = () => {
   const [videoSrc, setVideoSrc] = useState('/angry.mp4');
+  const [isLooping, setIsLooping] = useState(false);
 
   useEffect(() => {
     // 5초 후, sub 컴포넌트로 이동
     const timer = setTimeout(() => {
       setVideoSrc('/sub.mp4');
+      setIsLooping(true); // sub.mp4로 변경되었을때만 루프 설정
     }, 5000);
 
     // timer unmount
@@ -16,7 +18,7 @@ const Angry = () => {
   return (
     <>
       <div className="relative w-screen h-screen bg-white">
-        <video autoPlay preload='auto' src={videoSrc} className="absolute top-0 left-0 object-cover" />
+        <video autoPlay preload="auto" src={videoSrc} className="absolute top-0 left-0 object-cover" loop={isLooping} />
         <source src={videoSrc} type="video/mp4" />
       </div>
     </>
