ffe4167a83a5a3beed5cb191e3dffea128d3ac7f
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-08-24 18:03:53 +0900

    wlsWk

--- mwm/src/components/Camera.tsx
+++ mwm/src/components/Camera.tsx
@@ -2,26 +2,35 @@ import { useState, useEffect, useRef } from 'react';
 import { storage } from '../firebase';
 import { ref, getDownloadURL } from 'firebase/storage';
 import QRCode from 'qrcode.react';
+import { useRecoilValue } from 'recoil';
+import { time } from '../atom/currentTime';
+import { done } from '../atom/currentTime';
 
 // 웹 시작
 const MoziCamera = () => {
   // mergedImageURL에 이미지 URL 저장
   const [mergedImageURL, setMergedImageURL] = useState<string | null>(null);
   const qrCodeRef = useRef<any>(null);
+  const currentTime = useRecoilValue(time);
+  const doneValue = useRecoilValue(done);
 
   useEffect(() => {
-    // Firebase Storage에서 이미지 불러오기
-    const newStorage = storage;
-    const imageRef = ref(newStorage, 'images/mergedImage.jpg');
+    const fetchImageURL = async () => {
+      // Firebase Storage에서 이미지 불러오기
+      const newStorage = storage;
+      const imageRef = ref(newStorage, `images/mergedImage${currentTime}.jpg`);
 
-    getDownloadURL(imageRef)
-      .then((url) => {
-        setMergedImageURL(url);
-      })
-      .catch((error) => {
+      try {
+        if (doneValue) {
+          const url = await getDownloadURL(imageRef);
+          setMergedImageURL(url);
+        }
+      } catch (error) {
         console.error('이미지 불러오기 실패 : ', error);
-      });
-  }, []);
+      }
+    };
+    fetchImageURL();
+  }, [currentTime, doneValue]);
 
   useEffect(() => {
     if (qrCodeRef.current && mergedImageURL) {
@@ -31,6 +40,16 @@ const MoziCamera = () => {
     }
   }, [mergedImageURL]);
 
+  useEffect(() => {
+    // 5초 후, sub 컴포넌트로 이동
+    const timer = setTimeout(() => {
+      window.location.href = '/';
+    }, 20000);
+
+    // timer unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="relative justify-center items-center bg-white">
       <img src="/oziBack.png" alt="mozi" className="relative w-screen" />
@@ -38,7 +57,7 @@ const MoziCamera = () => {
       {mergedImageURL ? (
         <img src={mergedImageURL} alt="mergedImage" className="w-2/5 inset-y-72 inset-x-32 absolute" />
       ) : (
-        <p>이미지 불러오는 중...</p>
+        <p>image loading</p>
       )}
 
       <div className="absolute top-72 right-60">
